6e32e1e8c37f5d9c8c48f58c6058f02e97b534f9
LunaCI <lunaci@luadist.org>
2018-06-08 00:16:03 +0000

    Update to version 1.0-0

---
+++ main.lua
@@ -0,0 +1,17 @@
+local uuid4= require("src.uuid4")
+local uuid5= require("src.uuid5")
+
+--A v4 UUID
+print( uuid4.getUUID() )
+
+--SOME v5 UUIDs
+
+--An arbitary name
+print( uuid5.getUUID("A Name") )
+
+--A FQDN
+print( uuid5.getUUID("www.google.com","nsDNS") )
+
+--A URL
+print( uuid5.getUUID("http://en.wikipedia.org/Universally_unique_identifier","nsURL") )
+
